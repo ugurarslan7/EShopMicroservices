30fb7c8a7fee4c7ad84a713a7eed77aae5fe7447
Uğur ARSLAN <66887101+ugurarslan7@users.noreply.github.com>
2025-02-07 17:39:07 +0300

    Catalog Api - working on it

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+});
+var app = builder.Build();
+
+//Configure the HTTP request pipeline.
+app.MapCarter();
+
+app.Run();
