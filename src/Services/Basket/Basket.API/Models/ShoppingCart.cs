600cf0a34bc28d91c8851b80889759604b43873e
Uğur ARSLAN <66887101+ugurarslan7@users.noreply.github.com>
2025-03-27 17:21:17 +0300

    basket api and db completed.

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public ShoppingCart()
+    {
+            
+    }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    public string UserName { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } =new();
+    public decimal TotalPrice => Items.Sum(p => p.Quantity * p.Price);
+
+}
